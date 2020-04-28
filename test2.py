0b55eebd269a91f2f4cdaaab160e09ae30d9f7b9
ddkang <ddkang@pukyong.ac.kr>
2020-04-28 16:09:09 +0900

    통계값 계산 성공, 그림그려보자

--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@ import openpyxl as pyxl
 import pandas as pd
 import numpy as np
 
-#엑셀 파일(WorkBook) 읽기
+#프로그램 내에서 사용할 list 정의 (생성)
 
 time = list()
 pol_data = list()
@@ -15,13 +15,19 @@ SO = list()
 PM10 = list()
 PM25 = list()
 
+#프로그램 내에서 사용할 dictionary 정의 (생성)
+dic_mean = {}
+dic_max = {}
+dic_min = {}
+
+
 #관측 지점 목록
-# obs_site = ['광복동', '장림동', '학장동', '덕천동', '연산동', '대연동', '청룡동', 
-#      '전포동', '태종대','기장읍','대저동','부곡동','광안동','명장동','녹산동','용수리',
-#          '좌동','수정동','대신동','온천동','초량동']
+obs_site = ['광복동', '장림동', '학장동', '덕천동', '연산동', '대연동', '청룡동', 
+     '전포동', '태종대','기장읍','대저동','부곡동','광안동','명장동','녹산동','용수리',
+         '좌동','수정동','대신동','온천동','초량동']
 
 # test 
-obs_site = ['광복동', '장림동'] # test
+#obs_site = ['광복동', '장림동'] # test
 
 #일(day) 수 초기화
 num_day = 0
@@ -36,15 +42,15 @@ for t in range(1,25):
 # time.append((r[t].value)[:-1])
 
 writer = pd.ExcelWriter('result_test.xlsx') #output파일 생성
-
 # test # writer = pd.ExcelWriter('result_jeonpo.xlsx') #output파일 생성     
 
-for site_name in obs_site:
+
+for site_name in obs_site: #지점별로 loop
 
     for yr in range(2010, 2011):# 2017):
         for mn in range(1, 13): #  13):
 
-            if site_name == '학장동' and yr == 2010 and mn < 7 : #감전동 (구 학장동) 지점 고려
+            if site_name == '학장동' and yr == 2010 and mn < 7 : #감전동 (구 학장동) 지점 교체된 것 고려
                 site_name = '감전동'
             elif site_name == '감전동' and yr == 2010 and mn > 6:
                 site_name = '학장동'
@@ -163,37 +169,29 @@ for site_name in obs_site:
                         date.append(str(year)+str(month)+str(day)+str(time[i-1]))
                         # print(year, month, day, time[i-1], pol_data[i-1])
 
+    #지점별, 오염별 자료를 DataFrame 형태로 작성
     df = pd.DataFrame({'Date':pd.Series(tot_date),'O₃':pd.Series(O3), 'SO₂':pd.Series(SO), 'CO':pd.Series(CO), 'NO₂':pd.Series(NO),'PM-10':pd.Series(PM10), 'PM-2.5':pd.Series(PM25)})
     
     print(df)
 
-    df.to_excel(writer, sheet_name=site_name)
+    df.to_excel(writer, sheet_name=site_name) #엑셀 결과로 저장
    
+   #각 오염별 평균, 최대, 최소값을 계산하고 정의해둔 dictionary에 저장
     mean_value = df.mean(skipna=True)
+
+    max_value = df.max(skipna=True)
+
+    min_value = df.min(skipna=True)
+
+    dic_mean[site_name] = mean_value
+
+    dic_max[site_name] = max_value
+
+    dic_min[site_name] = min_value
 
     writer.save()
 
-   # 통계치 계산해서 저장할 결과 파일 만들기
-
-    list_pol = ['O₃', 'SO₂', 'CO', 'NO₂','PM-10','PM-2.5']
-
-    result_read = pyxl.load_workbook(writer)
-
-    #real_result = pd.ExcelWriter('test_mean.xlsx')
-
-    ws = result_read[site_name]
-
-    n = 10
-
-    for data in list_pol:
-        ws.cell(row=1, column=n).value = list_pol[n-10]
-        ws.cell(row=2, column=n).value = mean_value[data]
-
-        n = n + 1
-    
-    #result_read.save('test_mean.xlsx')
-    result_read.save('result_test.xlsx')
-
+    #list들 초기화
     tot_date = list()
     O3 = list()
     NO = list()
@@ -206,3 +204,41 @@ for site_name in obs_site:
 #df.set_index('date', inplace=True)
 #print(df)
 
+
+#통계치 계산해서 저장할 결과 파일 만들기
+
+list_pol = ['O₃', 'SO₂', 'CO', 'NO₂','PM-10','PM-2.5']
+
+result_read = pyxl.Workbook()
+
+ws_mean = result_read.create_sheet('average',1)
+ws_max =  result_read.create_sheet('max',2)
+ws_min =  result_read.create_sheet('min',3)
+
+n = 1
+
+for data in list_pol:
+    ws_mean.cell(row=1, column=n+1).value = list_pol[n-1]
+    ws_max.cell(row=1, column=n+1).value = list_pol[n-1]
+    ws_min.cell(row=1, column=n+1).value = list_pol[n-1]
+
+    n = n + 1
+
+m = 2
+o = 1
+
+for name in obs_site:
+    ws_mean.cell(row=m, column=1).value = name
+    ws_max.cell(row=m, column=1).value = name
+    ws_min.cell(row=m, column=1).value = name
+    for data in list_pol:
+        ws_mean.cell(row=m, column=o+1).value = dic_mean[name][data]
+        ws_max.cell(row=m, column=o+1).value = dic_max[name][data]
+        ws_min.cell(row=m, column=o+1).value = dic_min[name][data]
+
+        o = o + 1
+    o = 1
+
+    m = m + 1
+    
+result_read.save('result_statistics.xlsx')
