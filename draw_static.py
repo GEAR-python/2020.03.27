e315036e5ab2dff7f9f7260f469fcbb855330116
ddkang <ddkang@pukyong.ac.kr>
2020-04-28 18:06:47 +0900

    그림 그리는중

--- draw_static.py
+++ draw_static.py
@@ -1,2 +1,30 @@
 import pandas as pd
-from matplotlib import pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import openpyxl as pyxl
+
+#rb = pyxl.load_workbook("result_statistics.xlsx", )
+#rb = pd.read_excel("result_statistics.xlsx")
+
+sheet_names = pd.ExcelFile("result_statistics.xlsx").sheet_names
+
+dfs = {}
+
+for sheet in sheet_names:
+        dfs[sheet] = pd.read_excel("result_statistics.xlsx", sheet)
+
+ws = dfs['average']
+
+ = ws['O₃']
+
+
+
+sns.regplot(x)
+
+
+print()
+
+#ws = rb['average'] #min max
+
+#print(ws.head())
